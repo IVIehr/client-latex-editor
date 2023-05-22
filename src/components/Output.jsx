50abc6d7a8a52929a0d3f5a868c678dca2965908
m.navidimehr <mehr910093@gmail.com>
2023-05-22 15:08:44 +0430

    Set output direction based on direction of input

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,19 +1,41 @@
 import React from "react";
 import { parse, HtmlGenerator } from "latex.js";
+import stringDirection from "string-direction";
 
 const Output = ({ content }) => {
-  let latex =
-    "\\documentclass{article}\n\n\\begin{document}\n\n\\section{Introduction}\nThis is a sample LaTeX document.\n\n\\subsection{Subsection}\nHere is some text in a subsection.\n\n\\end{document}";
   let generator = new HtmlGenerator({ hyphenate: false });
 
-  let doc = parse(content, { generator: generator }).htmlDocument();
+  let outputToHTML = parse(content, { generator: generator }).htmlDocument();
+  let parsedHTML = new XMLSerializer().serializeToString(outputToHTML);
+
+  const extractText = (str) => {
+    var span = document.createElement("span");
+    span.innerHTML = str;
+    return span.textContent || span.innerText;
+  };
+
+  const finalHTML = (parsedHTML) => {
+    let direction = extractText(content);
+    const HTMLDirection =
+      stringDirection.getDirection(direction) === "ltr" ? "ltr" : "rtl";
+
+    // Add direction the the iframe html
+    let index = parsedHTML.indexOf("style") + "style= ".length;
+
+    return (
+      parsedHTML.slice(0, index) +
+      `direction:${HTMLDirection}; ` +
+      parsedHTML.slice(index)
+    );
+  };
+
   return (
     <div className="h-screen bg-violet-50 p-4">
       <iframe
         className="output-frame w-full h-full border-0"
         title="Output"
         sandbox="allow-same-origin allow-scripts"
-        srcDoc={new XMLSerializer().serializeToString(doc)}
+        srcDoc={finalHTML(parsedHTML)}
       ></iframe>
     </div>
   );
